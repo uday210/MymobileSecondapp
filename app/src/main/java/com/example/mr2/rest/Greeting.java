d4dc758de4b5fd2df25e1e786ac1ad9a87aa63d1
uday <udaykiran.bussa@globalnest.com>
2016-10-01 13:06:49 +0530

    first commit

---
+++ app/src/main/java/com/example/mr2/rest/Greeting.java
@@ -0,0 +1,17 @@
+package com.example.mr2.rest;
+
+/**
+ * Created by MR2 on 5/22/2016.
+ */
+public class Greeting {
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
